eaf50c495d32a103987ce26f8711183f04b5e03e
Gaurav Rajput <105104456+Gauravraj1141@users.noreply.github.com>
2022-11-15 12:32:57 +0530

    ssk

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,5 +1,8 @@
 
 from django.urls import path 
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 from . import views
 
@@ -9,4 +12,4 @@ urlpatterns = [
     path("delete/<td>/", views.deleteTask, name="delete_task"),
     path("<int:upid>", views.updatetask, name="update_task")
     
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATICFILES_DIR)
