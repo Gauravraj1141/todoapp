a44d8aa9492607d55c1269e0c7d1153188b38910
Gaurav Rajput <105104456+Gauravraj1141@users.noreply.github.com>
2022-11-15 12:02:20 +0530

    first todo

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path("", views.todos , name="todos"),
     path("tasks", views.tasks , name="tasks"),
     path("delete/<td>/", views.deleteTask, name="delete_task"),
-    path("<upid>", views.updatetask, name="update_task")
+    path("<int:upid>", views.updatetask, name="update_task")
     
-
 ]
